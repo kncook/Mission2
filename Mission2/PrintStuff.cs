67189b186d20d060ce0e53fdd5c6300f42392075
kncook <katherine.n.cook3@gmail.com>
2023-01-17 16:01:28 -0700

    Add project files.

---
+++ Mission2/PrintStuff.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mission2
+{
+    class PrintStuff
+    {
+        // Finding Random Number 1 and 2 --static int instead of void
+        public void GetRandomNum(int diceRoll)
+        {
+            int[] totals = new int[13];
+
+            for (int i = 0; i < diceRoll; i++)
+            {
+                int diceTotal = 0;
+                Random random = new Random();
+                int dice1 = random.Next(1, 7);   // creates a number between 1 and 6 
+                int dice2 = random.Next(1, 7);
+
+                // Adding Random Numbers Together
+                diceTotal = dice1 + dice2;
+
+                totals[(diceTotal)]++;
+   
+                
+        
+            }
+
+            // print histogram
+            for (int i = 2; i <= 12; i++)
+            {
+                Console.Write("{0}: ", i);
+                // calculate percentage and scale to number of asterix
+                int asterix = (int)((double)totals[i] / diceRoll * 100); 
+                for (int j = 0; j < asterix; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            
+            }
+
+
+            //PRINT
+            //for (int i = 0; i < diceRoll; i++)
+            //{
+            //  int NumPct = 0;
+            //NumPct = (char)(i + 12);
+
+            // Console.WriteLine(NumPct + ": " + totals[i]);
+            //}
+
+        }
+    }
+}
---
+++ Mission2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mission2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PrintStuff ps = new PrintStuff();
+
+            //initialize values
+            string userinput = "";
+            int diceRoll;
+
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            //allows user to inputand converts input to an int
+            userinput = Console.ReadLine();
+            diceRoll = Convert.ToInt32(userinput);
+
+            //prints at end
+            Console.WriteLine("\n" + "DICE ROLLING SIMULATION RESULTS" + "\n" + "Each '*' represents 1 % of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + diceRoll + "\n");
+
+            ps.GetRandomNum(diceRoll);
+        }
+    }
+}
